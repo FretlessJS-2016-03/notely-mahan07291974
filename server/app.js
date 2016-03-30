8fdc89c2604b5c0acdc0a3a5ddb6a453ce53064a
Matt Mahan <mmahan@stonegatemtg.com>
2016-03-29 20:13:33 -0400

    serve statice notes

--- server/app.js
+++ server/app.js
@@ -2,7 +2,16 @@ var express = require('express');
 var notelyServerApp = express();
 
 notelyServerApp.get('/', function(req, res) {
-  res.send('I <3 Express!');
+  res.json([
+    {
+      title: 'Hardcoded note',
+      body_html: 'Cool note.  Aww shucks.'
+    },
+    {
+      title: 'Anothardcoded note',
+      body_html: "Ain't life grand?"
+    }
+  ]);
 });
 
 notelyServerApp.listen(3030, function()
