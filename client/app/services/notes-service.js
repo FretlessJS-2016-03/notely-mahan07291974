c4749957cb0a8d4a099a1ee08c2807b5454fc78c
Matt Mahan <mmahan@stonegatemtg.com>
2016-03-30 17:12:27 -0400

    Use then rather than success

--- client/app/services/notes-service.js
+++ client/app/services/notes-service.js
@@ -1,21 +1,29 @@
 (function() {
-     angular.module('notely')
-       .service('NotesService', NotesService);
+  angular.module('notely')
+    .service('NotesService', NotesService);
 
-     NotesService.$inject = ['$http'];
-     function NotesService($http) {
-       var _this = this;
-       _this.notes = [];
+  NotesService.$inject = ['$http'];
+  function NotesService($http) {
+    var _this = this;
+    _this.notes = [];
 
-       _this.fetch = function() {
-         return $http.get('http://localhost:3030')
-           .success(function(notesData) {
-             _this.notes = notesData;
-           });
-       };
+    _this.fetch = function() {
+      return $http.get('http://localhost:3030')
+        .then(
+          // Success
+          function(response) {
+            _this.notes = response.data;
+          },
 
-       _this.getNotes = function() {
-         return _this.notes;
-       };
-     }
-   }());
+          // Failure
+          function(response) {
+            console.log('aww, snap:' + response);
+          }
+        );
+    };
+
+    _this.getNotes = function() {
+      return _this.notes;
+    };
+  }
+}());
